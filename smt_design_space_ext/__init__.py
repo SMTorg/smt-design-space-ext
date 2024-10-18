12f882dfe73ce73ac771eeac809e7f224e843f7d
relf <remi.lafage@onera.fr>
2024-10-18 12:07:16 +0200

    Expose only required symbols to work with smt at module level

--- smt_design_space_ext/__init__.py
+++ smt_design_space_ext/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
+
+# Symbols imported in smt to handle hierarchical variables
+from smt_design_space_ext.design_space import (
+    DesignSpace,
+    ensure_design_space,
+)
+
+__all__ = ["DesignSpace", "ensure_design_space"]
