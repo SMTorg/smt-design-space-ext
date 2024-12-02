3f994a81d5f06be461a6b62efe1fbab2809d0862
Paul-Saves <paul.saves@alumni.enac.fr>
2024-12-02 15:49:07 +0100

    fix import

--- smt_design_space_ext/tests/test_design_space.py
+++ smt_design_space_ext/tests/test_design_space.py
@@ -935,7 +935,7 @@ class Test(unittest.TestCase):
         )
 
     def test_smt(self):
-        from smt.applications import TestMixedInteger
+        from smt.applications.tests import TestMixedInteger
 
         test_smt_mi = TestMixedInteger()
         test_smt_mi.run_mixed_cs_example()
